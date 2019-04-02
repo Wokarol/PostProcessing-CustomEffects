234c975a77c8c92fb219dc6166f3ca694ce487be
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-04-02 20:44:49 +0200

    Finished image effect;

--- Assets/Scripts/InvertedColorsEffects/Inverted.cs
+++ Assets/Scripts/InvertedColorsEffects/Inverted.cs
@@ -6,11 +6,20 @@ using UnityEngine.Rendering.PostProcessing;
 [PostProcess(typeof(InvertedRenderer), PostProcessEvent.AfterStack, "Custom/Inverted")]
 public sealed class Inverted : PostProcessEffectSettings
 {
+    [Range(0f, 1f), Tooltip("Blend between normal and inverted color")]
+    public FloatParameter Blend = new FloatParameter { value = 0f };
 
+    public override bool IsEnabledAndSupported(PostProcessRenderContext context) {
+        return enabled.value && Blend.value > 0f;
+    }
 }
 
 public sealed class InvertedRenderer : PostProcessEffectRenderer<Inverted>
 {
+
     public override void Render(PostProcessRenderContext context) {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Inverted"));
+        sheet.properties.SetFloat("_Blend", settings.Blend);
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
 }
---
+++ Assets/Scripts/InvertedColorsEffects/Inverted.shader
@@ -0,0 +1,29 @@
+﻿Shader "Hidden/Custom/Inverted"
+{
+	HLSLINCLUDE
+		#include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+
+		TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+		float _Blend;
+
+		float4 Frag(VaryingsDefault i) : SV_Target
+		{
+			float4 c = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
+			return lerp(c, 1 - c, _Blend);
+		}
+
+	ENDHLSL
+
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			HLSLPROGRAM
+				#pragma vertex VertDefault
+				#pragma fragment Frag
+			ENDHLSL
+		}
+	}
+}
