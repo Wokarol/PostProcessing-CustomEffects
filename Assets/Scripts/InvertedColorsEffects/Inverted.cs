4ab0c927e0b03a1eefd90df9a7ece8a1a7489af6
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-04-02 20:06:45 +0200

    Defined Effect;

---
+++ Assets/Scripts/InvertedColorsEffects/Inverted.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(InvertedRenderer), PostProcessEvent.AfterStack, "Custom/Inverted")]
+public sealed class Inverted : PostProcessEffectSettings
+{
+
+}
+
+public sealed class InvertedRenderer : PostProcessEffectRenderer<Inverted>
+{
+    public override void Render(PostProcessRenderContext context) {
+    }
+}
